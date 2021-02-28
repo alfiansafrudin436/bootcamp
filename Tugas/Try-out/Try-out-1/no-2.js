6832d58c3e3c150bbe3e3708478427fb7924b551
alfiansafrudin436 <alfiansafrudin436@gmail.com>
2021-02-28 23:17:08 +0700

    Try Out 1

--- Tugas/Try-out/Try-out-1/no-2.js
+++ Tugas/Try-out/Try-out-1/no-2.js
@@ -1,9 +1,21 @@
-const axios = require('axios')
-const url='https://www.cgv.id/'
-axios.get(url)
-.then((res)=>{
-    console.log(res)
-})
-.catch((err)=>{
-    console.log(err)
-})
+const request = require('request');
+const cheerio = require('cheerio');
+
+const URL = "https://www.cgv.id/en/movies/info/21002000"; 
+request(URL, function (err, res, body) {
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        let $ = cheerio.load(body);
+        $('div.main-container>div.main-wrapper>div.main-body-container.skin>div.body-wrapper>div.movie-detail-body').each(function(index){
+            const title = $(this).find('div.movie-info-wrapper>div.movie-info-title').text();
+            const synopsis = $(this).find('div.movie-info-wrapper>div.synopsis-section>div.movie-synopsis.right').text();
+            const info = $(this).find('div.movie-info-wrapper>div.synopsis-section>div.movie-add-info.left').text();
+
+            console.log(title,synopsis, info)
+        });
+    }
+});
