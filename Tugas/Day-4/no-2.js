7f5d3b6de803c4fb057e0b011bd2416ee7db0c0c
alfiansafrudin436 <alfiansafrudin436@gmail.com>
2021-03-01 13:33:26 +0700

    mapping & finding object javascript

--- Tugas/Day-4/no-2.js
+++ Tugas/Day-4/no-2.js
@@ -4,14 +4,19 @@ axios.get(`https://jsonplaceholder.typicode.com/posts`)
 .then((result)=>{
     axios.get(`https://jsonplaceholder.typicode.com/users`)
     .then((res)=>{
-        for(i=0;i<result.data.length;i++){
-            for(j=0;j<res.data.length;j++){
-                if(result.data[i].id==res.data[j].id){
-                    Object.assign(result.data[i],{'user':res.data[j]})
-                    console.log(result.data[i])
-                }
-            }
-        }
+        // for(i=0;i<result.data.length;i++){
+        //     for(j=0;j<res.data.length;j++){
+        //         if(result.data[i].id==res.data[j].id){
+        //             Object.assign(result.data[i],{'user':res.data[j]})
+        //             console.log(result.data[i])
+        //         }
+        //     }
+        // }
+        result.data.map((e)=>{
+            const userData=res.data.find((u)=>u.id==e.userId)
+            e['user']=userData
+            console.log(e)
+        })
 
     })
     .catch((err)=>{
